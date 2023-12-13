75786ef993d90d98ecaa521bf7d3e578504c3ffb
meron6 <khozameron5@gmail.com>
2023-12-13 10:57:11 -0800

    2_shell

--- 2_shell.c
+++ 2_shell.c
@@ -1,25 +1,50 @@
-#include "holberton.h"
+#include "shell.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * interactive - returns true if shell is in interactive mode
+ * @info: struct address
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int main(void)
-{
-char *line;
-char **args;
-int status;
+int interactive(info_t *info) {
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
 
-do {
-printf("$ ");
-line = _getline();
-args = split_line(line);
-status = execute(args);
-
-free(line);
-free(args);
-} while (status);
-
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: the char to check
+ * @delim: the delimiter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim) {
+while (*delim) {
+if (*delim++ == c)
+return (1);
+}
 return (0);
+}
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c) {
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string to calculate length
+ * Return: length of the string
+ */
+int _strlen(char *s) {
+int length = 0;
+while (*s != '\0') {
+length++;
+s++;
+}
+return length;
 }
